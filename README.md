a18f7425459d16c2db239bab9e6a80f85179c816
Felipe da Veiga Leprevost <leprevost@cpan.org>
2014-10-10 15:23:33 +0000

    Import of LEPREVOST/MS2-Parser-0.06 from CPAN.
    gitpan-cpan-distribution: MS2-Parser
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         LEPREVOST/MS2-Parser-0.06.tar.gz
    gitpan-cpan-author:       LEPREVOST
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,4 @@
+MS2-Parser
+==========
+
+A Perl Parser for MS2 files, commonly used for mass spectrometry based proteomcis.
--- lib/MS2/Parser.pm
+++ lib/MS2/Parser.pm
@@ -14,11 +14,11 @@ MS2::Parser - A parser for MS2 files, commonly used in mass spectrometry based p
 
 =head1 VERSION
 
-Version 0.05'
+Version 0.06'
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
